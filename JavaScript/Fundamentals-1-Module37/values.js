f6e3cf3805010ebc2d7ddd1fd36ffff3dac9fac6
MD Danish <116485272+mddanish786786@users.noreply.github.com>
2024-03-27 05:04:04 +0000

    basics of JS

---
+++ JavaScript/Fundamentals-1-Module37/values.js
@@ -0,0 +1,36 @@
+// 7
+//8.9
+//Numbers
+
+//'Danish'
+//"Danish"
+//String
+
+//true
+//false
+//Boolean Values
+
+//null
+//undefined
+
+//premitive datatype
+//single value
+
+//non-premitive values=> multiple values
+//array => [] =>
+//object => {}
+//(), [], {}
+
+//["A","B", "C", "D", "E"]
+
+//{ }
+
+
+//__________________________
+
+//var
+//let
+//const
+
+//Learn about operators
+
